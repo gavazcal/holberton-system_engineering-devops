5bd9acb609efeb79fe231e16876a7c7e34731710
gavazcal <2392@holbertonschool.com>
2021-09-13 22:54:54 +0000

    adding task 0, working output

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    Uses REST API to return information about an employee
+"""
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    rest = 'https://jsonplaceholder.typicode.com/'
+    user_id = int(argv[1])
+    name = requests.get(rest + '/users/{}'.format(user_id)).json()
+    user_to_do = requests.get(rest + '/todos?userId={}'.format(user_id)).json()
+
+    done_tasks = []
+
+    for task in user_to_do:
+        if task.get('completed') is True:
+            done_tasks.append(task.get('title'))
+    print("Employee {} is done with tasks({}/{}):".
+            format(name.get('name'), len(done_tasks), len(user_to_do)))
+    for done in done_tasks:
+            print("\t {}".format(done))
