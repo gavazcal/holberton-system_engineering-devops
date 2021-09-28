3070e3e8852545f08c3323b0e0c420b4b5e4cc47
gavazcal <2392@holbertonschool.com>
2021-09-28 18:21:00 +0000

    testing task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" returns number of subs per subreddit """
+
+import json
+import requests
+
+def number_of_subscribers(subreddit):
+    """ returns number of subs per subreddit """
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    head = {'User-Agent': 'requested'}
+    response = requests.get(url, headers=head)
+    try:
+        if response.status_code != 200:
+            return 0
+        return response.json()['data']['subscribers']
+    except:
+        return 0
